b122765b5db6a5da04d0482da41120c8f4c0a0a0
veflex <alexandrelesimple75@gmail.com>
2018-08-18 15:44:20 +0200

    produits afficher manque nom des refs

--- app.js
+++ app.js
@@ -1,26 +1,64 @@
+/* global document*/
 var app = (function app() {
     "use strict";
 
     var dom = {},
         count = 0;
 
+    var CreateProduct = function (obj) {
+        this.name = obj.nom;
+        this.price = obj.prix;
+        this.descr = obj.description;
+        this.ref = obj.ref;
+    };
+
     var afficherForm = function () {
-        dom.form.classList.toggle("is-active")
-    }
-    var recupValues = function () {
-        if (dom.nom || dom.prix || dom.descr === null) {
-            alert("vous avez oubliez de remplir un champ produit")
-        }
-        count++;
-    }
+        dom.form.classList.toggle("is-active");
+    };
+    var afficherNewProducs = function () {
+        dom.newProds.classList.add("is-active");
+    };
 
     var ref = function () {
         if (count < 10) {
-            return "00" + count
+            return "00" + count;
         } else if (count < 100) {
-            return "0" + count
-        } else return count
-    }
+            return "0" + count;
+        } else return count;
+    };
+
+    var alertIfMissingValues = function () {
+        if (!dom.nom.value || !dom.prix.value || !dom.descr.value) {
+            alert("vous avez oubliez de remplir un champ produit")
+        } else {
+            count++;
+            return new CreateProduct({
+                nom: dom.nom.value,
+                prix: dom.prix.value,
+                description: dom.descr.value,
+                ref: ref()
+            });
+        }
+
+    };
+
+    const convertToArray = function convertToArray(obj) {
+        const tmp = [];
+        for (let prop in obj) {
+            tmp.push(obj[prop]);
+        }
+        return tmp;
+    };
+
+    var ecrireProducts = function () {
+        var prodTableau = convertToArray(alertIfMissingValues())
+        afficherNewProducs();
+        prodTableau.forEach(function (elem) {
+            dom.newProds.innerHTML += elem + "<br>";
+        })
+    };
+
+
 
 
 
@@ -32,11 +70,14 @@ var app = (function app() {
         dom.nom = document.getElementById('prodName');
         dom.prix = document.getElementById('prodPrice');
         dom.descr = document.getElementById('prodDescr');
+        dom.newProds = document.getElementById('newProds');
+        dom.prodList = document.getElementById('products');
+
 
 
         dom.creer.addEventListener("click", afficherForm);
 
-        dom.add.addEventListener("click", recupValues)
+        dom.add.addEventListener("click", ecrireProducts)
 
     };
     window.addEventListener("DOMContentLoaded", start);
