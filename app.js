32d38dce98c941fd25ccd495b7cc35992f5e455b
Your Name <you@example.com>
2018-08-20 16:53:26 +0200

    suppression par objet fonctionnel reste a modifier les objets

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ var app = (function app() {
     "use strict";
 
     var dom = {},
-        count = 0;
+        count = 0,
+        objectTabl = [];
 
     var CreateProduct = function (obj) {
+        this.modif = `<button id= ${obj.ref} >Modifier</button>`;
         this.name = obj.nom;
         this.price = obj.prix;
         this.descr = obj.description;
         this.ref = obj.ref;
-        this.suppr = "✖"
+        this.suppr = `<button id= ${obj.ref} >Supprimer</button>`;
     };
 
     var afficherForm = function () {
@@ -25,26 +27,32 @@ var app = (function app() {
             return "00" + count;
         } else if (count < 100) {
             return "0" + count;
-        } else return count;
-    };
-
-    var alertIfMissingValues = function () {
-        if (!dom.nom.value || !dom.prix.value || !dom.descr.value) {
-            alert("vous avez oubliez de remplir un champ produit")
         } else {
-            count++;
-            afficherNewProducs();
-            return new CreateProduct({
-                nom: dom.nom.value,
-                prix: dom.prix.value,
-                description: dom.descr.value,
-                ref: ref()
-            });
+            return count;
         }
     };
 
-    const convertToArray = function convertToArray(obj) {
-        const tmp = [];
+    var createProd = function () {
+        count += 1;
+        var newProd = new CreateProduct({
+            nom: dom.nom.value,
+            prix: dom.prix.value,
+            description: dom.descr.value,
+            ref: ref()
+        });
+        return newProd;
+    }
+    var alertIfMissingValues = function () {
+        if (!dom.nom.value || !dom.prix.value || !dom.descr.value) {
+            alert("vous avez oubliez de remplir un champ produit");
+        } else {
+            afficherNewProducs();
+            objectTabl.push(createProd());
+        }
+    };
+
+    var convertToArray = function convertToArray(obj) {
+        var tmp = [];
         for (let prop in obj) {
             tmp.push(obj[prop]);
         }
@@ -54,25 +62,55 @@ var app = (function app() {
     var ajouterLigneTableau = function () {
         var ligne = document.createElement("tr");
         return dom.tableau.appendChild(ligne);
-    }
-    var supprimerProduct = function () {
-        this.parentNode.remove();
-    }
-    var eventLastTd = function () {
-        dom.lastTd = document.querySelectorAll('#tbody td:last-child');
-        dom.lastTd.forEach(function (td) {
-            td.addEventListener("click", supprimerProduct)
-        })
-    }
+    };
+
+    var eventBtnSuppr = function () {
+        dom.lastTd = document.querySelectorAll('#tbody td:nth-child(6) button');
+        dom.lastTd.forEach(function (btn) {
+            btn.addEventListener("click", supprimerProduct);
+        });
+    };
+
+    //    var eventBtnModif = function () {
+    //        dom.firstTd = document.querySelectorAll('#tbody td:first-child button');
+    //        dom.firstTd.forEach(function (btn) {
+    //            btn.addEventListener("click", modifProduct);
+    //        });
+    //    };
 
     var ecrireProducts = function () {
-        var prodTableau = convertToArray(alertIfMissingValues())
-        var ligneCree = ajouterLigneTableau();
-        prodTableau.forEach(function (elem) {
-            var newTd = document.createElement("td");
-            ligneCree.appendChild(newTd).innerHTML = elem;
-            eventLastTd()
+        alertIfMissingValues();
+        dom.tableau.innerHTML = "";
+        var i;
+        for (i = 0; i < objectTabl.length; i++) {
+            var ligneCree = ajouterLigneTableau();
+            var propTabl = convertToArray(objectTabl[i]);
+            propTabl.forEach(function (obj) {
+                var newTd = document.createElement("td");
+                ligneCree.appendChild(newTd).innerHTML = obj;
+            });
+            eventBtnSuppr();
+            //            eventBtnModif();
+        }
+    };
+
+    var filtrerObjectTabl = function (idSuppr) {
+        var filteredTabl = objectTabl.filter(function (obj) {
+            return obj.ref != idSuppr
         })
+        return filteredTabl;
+    }
+
+    var supprimerProduct = function () {
+        var id = this["id"];
+        var btn = document.getElementById(id);
+        btn.parentNode.parentNode.remove();
+
+        window.console.log("tableau object avant");
+        window.console.log(objectTabl);
+        objectTabl = filtrerObjectTabl(id);
+        window.console.log("tableau object apres");
+        window.console.log(objectTabl);
     };
 
     var start = function () {
@@ -88,7 +126,7 @@ var app = (function app() {
 
         dom.creer.addEventListener("click", afficherForm);
 
-        dom.add.addEventListener("click", ecrireProducts)
+        dom.add.addEventListener("click", ecrireProducts);
 
     };
     window.addEventListener("DOMContentLoaded", start);
